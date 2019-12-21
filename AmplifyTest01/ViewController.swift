26f7ca9f01dfe750336c208675b07c36db353c8f
Keishin CHOU <keishin.chou@gmail.com>
2019-12-21 22:20:28 +0900

    Initial Commit

---
+++ AmplifyTest01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AmplifyTest01
+//
+//  Created by Keishin CHOU on 2019/12/21.
+//  Copyright © 2019 Keishin CHOU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
